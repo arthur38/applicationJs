b6ef8bb2adb7f9e61afce727d80f33f4602015a1
Joris Nicolas <Joris.Nicolas@e.ujf-grenoble.fr>
2015-11-09 17:21:20 +0100

    map google, mise en forme et ajout de donnée de l'API

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,26 @@
 (function(angular) {
   'use strict';
-  angular.module('myApp',['helloModule','contactModule', 'checkinModule']);
+  angular.module('myApp',
+    [
+      'ngRoute',
+      'ngMap',
+      
+      'helloModule',
+      'contactModule',
+      'checkinModule'
+    ])
+
+  .config(function($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+       templateUrl: 'assets/template/checkinList.html',
+     })
+     .when('/checkin/:checkinId', {
+      templateUrl: 'assets/template/checkinDetails.html',
+      controller: 'checkDetailsController'
+    });
+
+  });
+
 })(window.angular);
--- src/assets/js/controllers/checkin.js
+++ src/assets/js/controllers/checkin.js
@@ -2,14 +2,12 @@
   'use strict';
   angular.module('checkinModule',[])
     .controller('checkinController', function($scope, $http) {
-      console.log("check");
 
         $http({
           method: 'GET',
           url: 'http://checkin-api.dev.cap-liberte.com/checkin'
         }).then(function successCallback(response) {
-
-          console.log(response.data)
+          console.log(response);
           $scope.checkin = response.data;
 
         }, function errorCallback(response) {
@@ -17,5 +15,47 @@
           // or server returns response with an error status.
         });
 
+    })
+
+    .controller('checkDetailsController', function($scope, $http, $routeParams) {
+
+      $http({
+        method: 'GET',
+        url: 'http://checkin-api.dev.cap-liberte.com/checkin/' +  $routeParams.checkinId
+      }).then(function successCallback(response) {
+
+        $scope.checkinDetails = response.data;
+
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
+    })
+
+    .controller('checkinFormController', function($scope, $http) {
+        $scope.submit = function() {
+
+          $http({
+            method: 'POST',
+            url: 'http://checkin-api.dev.cap-liberte.com/checkin',
+            data : {
+              lat: $scope.lat,
+              lng: $scope.lng
+            },
+            headers: {
+              'Content-Type': undefined
+            }
+          }).then(function successCallback(response) {
+
+
+
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+        };
     });
+
+
 })(window.angular);
